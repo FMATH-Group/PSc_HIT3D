27807fd87735f6b4e59f770716c3dbba0ef0511e
aliakhav <akhavans@msu.edu>
2020-12-09 14:56:31 -0500

    Cleaning the code.

--- Gen_IC.py
+++ Gen_IC.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-from time import time
-from numpy import *
-from numpy.fft import fftfreq, fft, ifft, irfft2, rfft2
-from mpi4py import MPI
-
-
 import numpy as np
 from numpy import linalg as LA
 import sys
 import os
 from scipy import io
 
+from numpy.fft import fftfreq, fft, ifft, irfft2, rfft2
+from mpi4py import MPI
 
 try:
     from pyfftw.interfaces.numpy_fft import fft, ifft, irfft2, rfft2
