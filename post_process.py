d05f04e27aa82fdb53653af471f7dd847c435944
aliakhav <akhavans@msu.edu>
2020-12-09 17:26:52 -0500

    Taking input variables to initialize post-processing.

--- post_process.py
+++ post_process.py
@@ -19,3 +19,25 @@ except ImportError:
     pass
 
 # %%##########################################################################
+
+solver_type = sys.argv[-1]
+
+sim_in = sys.argv[1]
+IC_in = sys.argv[2]
+N_in = sys.argv[3]
+nout_in = sys.argv[4]
+
+
+inp = int(IC_in)
+N = int(N_in)
+
+K_cut = np.sqrt(2.0)/3.0
+Kmax = K_cut*N
+
+
+dirname = 'Sim_'+sim_in+'-IC_'+IC_in
+dirpath = os.path.join(os.getcwd(),dirname)
+rst = os.path.join(dirpath,'Out_'+nout_in)
+
+
+# %%##########################################################################
